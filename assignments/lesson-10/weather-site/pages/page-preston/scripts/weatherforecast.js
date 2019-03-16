3a9f23d4d9496b664f3109c4bf33414b32ad7856
Ronan Macedo Account <ronan.macedo.edu@gmail.com>
2019-03-15 23:23:20 -0300

    weatherinfo.js-fixed

--- assignments/lesson-10/weather-site/pages/page-preston/scripts/weatherforecast.js
+++
@@ -1,9 +0,0 @@
-var weatherForecast = new XMLHttpRequest;
-weatherForecast.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=d4cc538d72f36351ef3d7787b27c039e&units=imperial', true);
-weatherForecast.send();
-
-weatherRequest.onload = function () {
-    var forecast = JSON.parse(weatherForecast.responseText);
-    /* json information */
-    console.log(forecast);
-}
